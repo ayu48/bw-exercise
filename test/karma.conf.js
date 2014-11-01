11cc8812d202e7b5ccbabcafeaa4c6ef05435b29
blueteeth48 <ayuishii.blue+git@gmail.com>
2014-11-01 15:08:25 -0700

    fix test env

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: '../',
 
 
     // frameworks to use
@@ -15,7 +15,9 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '"test/**/*Spec.js"'
+      'dist/js/*.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'test/unit/**/*.js'
     ],
 
 
