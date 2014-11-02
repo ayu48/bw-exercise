38b60c7df8bb73057434e5a0526ff0d8da428a30
blueteeth48 <ayuishii.blue+git@gmail.com>
2014-11-01 23:00:15 -0700

    in-place modify elements instead of redrawing

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,13 +6,15 @@ angular.module('BWProgress', []).directive('progressIndicator', function($parse)
       actual: "="
     },
     link: function(scope, element, attr) {
-      var drawProgressIndicator, isValid, svg;
+      var actual, arc, arcs, color, elem, expected, getPercentage, isValid, progressNum, svg, updateIndicator, updateText;
+      expected = 0;
+      actual = 0;
       scope.$watchCollection('[expected, actual]', function(_arg) {
         var actual, expected;
         expected = _arg[0], actual = _arg[1];
         if (isValid(expected) && isValid(actual)) {
-          svg.selectAll("*").remove();
-          return drawProgressIndicator(expected, actual);
+          updateIndicator(expected, actual);
+          return updateText(expected, actual);
         }
       });
       isValid = function(value) {
@@ -21,36 +23,55 @@ angular.module('BWProgress', []).directive('progressIndicator', function($parse)
         }
         return false;
       };
+      getPercentage = function(expected, actual) {
+        return Math.round(actual / expected * 100);
+      };
       svg = d3.select(element[0]).append('svg');
-      return drawProgressIndicator = function(expected, actual) {
-        var arcs, color, elem, percentage, progressNum;
-        percentage = Math.round(actual / expected * 100);
-        elem = svg.append("g").attr("class", "circle-translate");
-        elem.append("circle").attr("r", 80);
-        progressNum = elem.append("text").attr("class", "progress-num").attr("dx", -40).attr("dy", 15).attr("textLength", "80px").text(percentage);
-        progressNum.append("tspan").text("%");
-        elem.append("text").attr("class", "progress-text").attr("dx", -35).attr("dy", 40).attr("textLength", "80px").text('Progress');
-        arcs = [
-          {
-            "class": "actual-arc circle-translate",
-            innerRadius: 90,
-            outerRadius: 93,
-            endAngle: parseFloat(actual)
-          }, {
-            "class": "expected-arc circle-translate",
-            innerRadius: 95,
-            outerRadius: 100,
-            endAngle: parseFloat(expected)
-          }
-        ];
-        color = d3.scale.linear().domain([0, 60, 100]).range(["#D91500", "#FFBA00", "#60CC00"]);
-        return svg.selectAll("path.arc").data(arcs).enter().append("path").attr("class", function(d) {
-          return d["class"];
-        }).transition().duration(800).attrTween("d", function(d) {
-          return d3.svg.arc().innerRadius(d.innerRadius).outerRadius(d.outerRadius).startAngle(0).endAngle(function(t) {
-            return t * (d.endAngle / 1) * 2 * Math.PI;
-          });
-        }).attr('fill', color(percentage));
+      elem = svg.append("g").attr("class", "circle-translate");
+      elem.append("circle").attr("r", 80);
+      progressNum = elem.append("text").attr("class", "progress-num").attr("dx", -40).attr("dy", 15).attr("textLength", "85px").text(getPercentage(expected, actual));
+      progressNum.append("tspan").text("%");
+      elem.append("text").attr("class", "progress-text").attr("dx", -35).attr("dy", 40).attr("textLength", "80px").text('Progress');
+      arcs = [
+        {
+          "class": "actual-arc circle-translate",
+          innerRadius: 90,
+          outerRadius: 93,
+          endAngle: parseFloat(actual)
+        }, {
+          "class": "expected-arc circle-translate",
+          innerRadius: 95,
+          outerRadius: 100,
+          endAngle: parseFloat(expected)
+        }
+      ];
+      color = d3.scale.linear().domain([0, 60, 100]).range(["#D91500", "#FFBA00", "#60CC00"]);
+      arc = d3.svg.arc().innerRadius(function(d) {
+        return d.innerRadius;
+      }).outerRadius(function(d) {
+        return d.outerRadius;
+      }).startAngle(0).endAngle(function(d) {
+        return (d.endAngle / 1) * 2 * Math.PI;
+      });
+      svg.selectAll("path.arc").data(arcs).enter().append("path").attr("class", function(d) {
+        return d["class"];
+      }).attr('fill', color(getPercentage(expected, actual))).transition().duration(800).attrTween("d", function(d) {
+        return arc(d);
+      });
+      updateText = function(expected, actual) {
+        return progressNum.text(getPercentage(expected, actual)).append("tspan").text("%");
+      };
+      return updateIndicator = function(expected, actual) {
+        var newValue;
+        newValue = [actual, expected];
+        return svg.selectAll("path").transition().duration(800).attr('fill', color(getPercentage(expected, actual))).attrTween("d", function(d, i) {
+          var interpolate;
+          interpolate = d3.interpolate(d.endAngle, newValue[i]);
+          return function(t) {
+            d.endAngle = interpolate(t);
+            return arc(d);
+          };
+        });
       };
     }
   };
--- src/scripts/main.coffee
+++ src/scripts/main.coffee
@@ -6,75 +6,91 @@ angular.module('BWProgress', [])
         expected: "="
         actual: "="
       link: (scope, element, attr) ->
+        expected = 0
+        actual = 0
 
         scope.$watchCollection '[expected, actual]', ([expected, actual]) ->
           if isValid(expected) and isValid(actual)
-            svg.selectAll("*").remove()
-            drawProgressIndicator(expected, actual)
+            updateIndicator(expected, actual)
+            updateText(expected, actual)
 
         isValid = (value) ->
           if (value and !isNaN(value) and value <= 1 and value >= 0)
             return true
           return false
 
+        getPercentage = (expected, actual) -> Math.round(actual/expected * 100)
+
         svg = d3.select(element[0]).append('svg')
+        elem = svg.append("g").attr("class", "circle-translate");
 
-        drawProgressIndicator = (expected, actual) ->
-          percentage = Math.round(actual/expected * 100)
+        #inner circle
+        elem.append("circle").attr("r", 80)
 
-          elem = svg.append("g")
-            .attr("class", "circle-translate")
+        #inner circle
+        progressNum = elem.append("text")
+          .attr("class", "progress-num")
+          .attr("dx", -40)
+          .attr("dy", 15)
+          .attr("textLength", "85px")
+          .text(getPercentage(expected, actual))
 
-          #inner circle
-          elem.append("circle")
-            .attr("r", 80)
+        progressNum.append("tspan").text("%")
 
-          progressNum = elem.append("text")
-            .attr("class", "progress-num")
-            .attr("dx", -40)
-            .attr("dy", 15)
-            .attr("textLength", "80px")
-            .text(percentage)
+        elem.append("text")
+          .attr("class", "progress-text")
+          .attr("dx", -35)
+          .attr("dy", 40)
+          .attr("textLength", "80px")
+          .text('Progress')
 
-          progressNum.append("tspan")
-            .text("%")
+        #arcs
+        arcs = [
+          {
+            class: "actual-arc circle-translate"
+            innerRadius: 90,
+            outerRadius: 93,
+            endAngle: parseFloat(actual)
+          },
+          {
+            class: "expected-arc circle-translate"
+            innerRadius: 95,
+            outerRadius: 100,
+            endAngle: parseFloat(expected)
+          }
+        ]
 
-          elem.append("text")
-            .attr("class", "progress-text")
-            .attr("dx", -35)
-            .attr("dy", 40)
-            .attr("textLength", "80px")
-            .text('Progress')
+        color = d3.scale.linear().domain([0, 60, 100]).range(["#D91500","#FFBA00", "#60CC00"])
 
-          arcs = [
-            {
-              class: "actual-arc circle-translate"
-              innerRadius: 90,
-              outerRadius: 93,
-              endAngle: parseFloat(actual)
-            },
-            {
-              class: "expected-arc circle-translate"
-              innerRadius: 95,
-              outerRadius: 100,
-              endAngle: parseFloat(expected)
-            }
-          ]
+        arc = d3.svg.arc()
+          .innerRadius((d) -> d.innerRadius)
+          .outerRadius((d) -> d.outerRadius)
+          .startAngle(0)
+          .endAngle((d) -> (d.endAngle/1) * 2 * Math.PI)
 
-          color = d3.scale.linear().domain([0, 60, 100]).range(["#D91500","#FFBA00", "#60CC00"])
+        svg.selectAll("path.arc").data(arcs)
+          .enter().append("path")
+          .attr("class", (d) -> d.class)
+          .attr('fill', color(getPercentage(expected, actual)))
+          .transition().duration(800)
+          .attrTween("d", (d) ->
+            return arc(d)
+          )
 
-          svg.selectAll("path.arc").data(arcs)
-            .enter().append("path")
-            .attr("class", (d) -> d.class)
-            .transition().duration(800)
-            .attrTween("d", (d) ->
-              return d3.svg.arc()
-                .innerRadius(d.innerRadius)
-                .outerRadius(d.outerRadius)
-                .startAngle(0)
-                .endAngle((t) -> t * (d.endAngle/1) * 2 * Math.PI)
-            ).attr('fill', color(percentage))
+        updateText = (expected, actual) ->
+          progressNum.text(getPercentage(expected, actual))
+            .append("tspan").text("%")
 
+        updateIndicator = (expected, actual) ->
+          newValue = [actual, expected]
+          svg.selectAll("path").transition().duration(800)
+            .attr('fill', color(getPercentage(expected, actual)))
+            .attrTween("d", (d, i) ->
+              interpolate = d3.interpolate(d.endAngle, newValue[i])
+              return (t) ->
+                d.endAngle = interpolate(t)
+                return arc(d)
+            )
     }
   )
 
